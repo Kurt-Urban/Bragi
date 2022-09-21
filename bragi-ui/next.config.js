2048c279b734eb2477458448f8fd8e84243e731d
Kurt-Urban <kurturban00@gmail.com>
2022-09-20 22:29:11 -0400

    feat: added image domain to nextconfig

--- bragi-ui/next.config.js
+++ bragi-ui/next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/query",
-        destination: "http://localhost:4000",
-      },
-      {
-        source: "/lyrics",
-        destination: "http://localhost:4000/lyrics",
-      },
-    ];
+  images: {
+    domains: ["images.genius.com"],
   },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/query",
+  //       destination: "http://localhost:4000",
+  //     },
+  //     {
+  //       source: "/lyrics",
+  //       destination: "http://localhost:4000/lyrics",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;

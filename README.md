3c6deb0836aaa7565ea7c79357ae03aba9991dc9
Kurt-Urban <kurturban00@gmail.com>
2022-10-06 22:06:59 -0400

    feat: final prep and instructions for submission

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Bragi
+
 Norse god of music
 
 Simple GUI and Server to search for songs and their lyrics.
 Project for SWENG861 - Penn State Masters Program
+
+## Getting Started
+
+Open two terminals and run the following commands in each:
+
+Server:
+
+```
+cd bragi-server
+npm install | yarn
+npm start | yarn start
+```
+
+UI
+
+```
+cd bragi-ui
+npm install | yarn
+npm run dev | yarn dev
+```
--- bragi-server/src/index.ts
+++ bragi-server/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import { lyricsTest, songListTestData } from "./testData";
+// import { lyricsTest, songListTestData } from "./testData";
 
 const cors = require("cors");
 const app = express();
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+  console.info(`Running on port ${port}`);
 });
 
 // Get song list based on the query string
@@ -21,13 +21,14 @@ app.post("/", (req: Request, res: Response) => {
   const options = {
     params: { q: req.body.query },
     headers: {
+      // REPLACE THIS WITH YOUR API KEY FROM RAPIDAPI GENIUSAPIKEY
       "X-RapidAPI-Key": process.env.API_KEY,
       "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
     },
   };
   axios
     .get("https://genius-song-lyrics1.p.rapidapi.com/search", options)
-    .then(function (response) {
+    .then((response) => {
       if (response.data.meta.status === 200) {
         return res.send(
           response.data.response.hits.map((hit: any) => hit.result)
@@ -38,7 +39,7 @@ app.post("/", (req: Request, res: Response) => {
         );
       }
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.error(error);
     });
   // Uncomment this line to use test data
@@ -56,6 +57,7 @@ app.post("/lyrics", (req: Request, res: Response) => {
     method: "GET",
     url: `https://genius-song-lyrics1.p.rapidapi.com/songs/${req.body.id}/lyrics`,
     headers: {
+      // REPLACE THIS WITH YOUR API KEY FROM RAPIDAPI GENIUSAPIKEY
       "X-RapidAPI-Key": process.env.API_KEY,
       "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
     },
@@ -63,7 +65,7 @@ app.post("/lyrics", (req: Request, res: Response) => {
 
   axios
     .request(options)
-    .then(function (response) {
+    .then((response) => {
       if (response.data.meta.status === 200) {
         return res.send(response.data.response.lyrics);
       } else {
@@ -72,7 +74,7 @@ app.post("/lyrics", (req: Request, res: Response) => {
         );
       }
     })
-    .catch(function (error) {
+    .catch((error) => {
       res.send(error.message);
     });
 
